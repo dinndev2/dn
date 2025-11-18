89ff2b154433d8ad9651c41760d930d4ed2cefb7
Dinndev <dinndev2@gmail.com>
2025-11-18 18:37:28 +0800

    To public svgs

--- src/components/icons/Dinndev.jsx
+++ src/components/icons/Dinndev.jsx
@@ -1,5 +1,5 @@
 export default function Dinndev() {
   return (
-    <img src="/public/dinndev.svg" alt="dinndev" className="h-8 sm:h-10 md:h-12 w-auto" />
+    <img src="/dinndev.svg" alt="dinndev" className="h-8 sm:h-10 md:h-12 w-auto" />
   )
 }
